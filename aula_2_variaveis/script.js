bd6482807e33ad0477ef4c7350c434217f1adede
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-01-23 21:56:56 -0300

    aulas java script sintaxes, variaveis e constante

---
+++ aula_2_variaveis/script.js
@@ -0,0 +1,11 @@
+//tipos primitivos
+
+//tipo booleano
+var VOuF = false;
+
+console.log(typeof(VOuF));
+
+var numeroqualquer = 1;
+
+//console.log(typeof(numeroqualquer));
+console.log(numeroqualquer);
